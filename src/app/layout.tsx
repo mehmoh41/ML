6598e4dac6856f54eaa08340567ed2418a34c05b
Mehdi Mohammadi <Moh.meh41@gmail.com>
2025-09-14 08:36:10 +0000

    in the public folder I have uploaded an image named as favicon, make it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
