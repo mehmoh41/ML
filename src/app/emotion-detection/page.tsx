5ce85536428018a70bd9cafef56548c25eed8ab3
Mehdi Mohammadi <Moh.meh41@gmail.com>
2025-09-15 01:46:58 +0000

    remove this part from emotion detection: You can also 'train' the model

--- src/app/emotion-detection/page.tsx
+++ src/app/emotion-detection/page.tsx
@@ -10,7 +10,7 @@ export default function EmotionDetectionPage() {
       <div className="container mx-auto max-w-5xl px-4 py-8 md:py-12">
         <PageHeader
           title="Emotion Detection"
-          description="Use your webcam to detect emotions in real-time. You can also 'train' the model by capturing samples of your expressions to improve its (simulated) accuracy."
+          description="Use your webcam to detect emotions in real-time."
         />
         <EmotionDetectionView />
       </div>
